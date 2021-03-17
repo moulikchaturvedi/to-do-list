f384bd935c896015aef418a3287994877746eb18
Moulik Chaturvedi <moulik.chaturvedi26@gmail.com>
2021-03-17 10:02:32 +0530

    Started with php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    <title>To-do List</title>
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+<body>
+    <form action="./addtask.php" method="POST">
+        <input type="text" name="inputtask" placeholder="Enter a New Task">
+        <button>ADD TASK</button>
+    </form>
+</body>
+</html>
